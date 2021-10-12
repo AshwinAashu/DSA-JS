be57afbb3442d39e1918d3f495a70cf6a74c5f75
AshwinAashu <80351416+AshwinAashu@users.noreply.github.com>
2021-10-12 17:44:17 +0530

    Leetcode DS  day1

--- areThereDuplicates.js
+++ areThereDuplicates.js
@@ -9,10 +9,12 @@
 
 
 //Approach - used arguments object to solve this. created a set of unique values and matched with the length of the arguments
-function areThereDuplicates(){
-  return new Set(arguments).size !== arguments.length;
+function areThereDuplicates(nums){
+  return new Set(nums).size !== nums.length;
+
 }
 
 console.log(areThereDuplicates(1,5,9,5));
-console.log(areThereDuplicates(1,3,2));
-console.log(areThereDuplicates(1,2,2));
+console.log(areThereDuplicates(1,9,5));
+// console.log(areThereDuplicates(1,3,2));
+// console.log(areThereDuplicates(1,2,2));
